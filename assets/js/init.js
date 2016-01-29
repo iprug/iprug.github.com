6b07194c5ac5821f602312fd841edf618d353790
Kerry Buckley <kerryjbuckley@gmail.com>
2016-01-29 19:39:14 +0000

    Remove auto date header stuff

--- assets/js/init.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  var today = new Date();
-  $(".page-header").append('<p class="next-date">Next meeting: ' +
-    IPRUG.nextDate(today).toString("dddd d MMMM") +
-    IPRUG.extraDateMessage(today) + '</p>');
-});
-
